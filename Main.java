c479fae1acc34ac480a3201b2cd2824eac7e90e8
Student <maciek.galazka2004@gmail.com>
2024-10-12 18:20:42 +0200

    first

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
- public class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+
+        System.out.println("Podana liczba to: " + inputNumber);
+
+    if (inputNumber % 2 == 0){
+        System.out.println("Liczba jest parzysta");
+    }
+    else {
+        System.out.println("Liczba jest nieparzysta");
+    }
+
     }
 }
